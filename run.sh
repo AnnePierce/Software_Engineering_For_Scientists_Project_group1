44e85689e02ed0109445eed95cadc160d58eb0b8
Caroline Howell <89031663+wpcaroline@users.noreply.github.com>
2021-11-14 12:11:38 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /opt/conda/etc/profile.d/conda.sh
-python GRABDA_freesocialinteraction.py /home/jovyan/swefs_group1/correct_test_data/FiberPhoSig2020-08-22T09_00_59.csv
+python parsing.py /home/jovyan/swefs_group1/correct_test_data/FiberPhoSig2020-08-22T09_00_59.csv
