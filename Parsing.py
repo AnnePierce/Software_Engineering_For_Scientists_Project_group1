d6c9952e46bad1649d7d389723263b2654708ad5
Caroline Howell <89031663+wpcaroline@users.noreply.github.com>
2021-11-15 07:03:36 -0700

    Update Parsing.py

--- Parsing.py
+++ Parsing.py
@@ -1,12 +1,12 @@
-def file_reader(filename): 
+def file_reader(filename):
     # Import Data and deinterleave
-    
+
     fdata = open(filename)
     # Load file containing florescence data
-    f_time = fdata.strip().split() 
-    # Pull out red and green florescence for each vole 
-    ftime = f_time[0] 
-    # Parse the time for each florescence 
+    f_time = fdata.strip().split()
+    # Pull out red and green florescence for each vole
+    ftime = f_time[0]
+    # Parse the time for each florescence
     fgreen2 = f_time[2]
     # Parses green florescence for vole 2 (stranger or partner)
     fgreen1 = f_time[3]
@@ -15,22 +15,31 @@ def file_reader(filename):
     # Parses red florescenece for vole 2 (stranger or partner)
     fred1 = f_time[5]
     # Parses red florescence for vole 1 (subject)
-    
-    ftime = f_time[0] / (60*1000) 
+
+    ftime = f_time[0] / (60*1000)
     # Converts ftime from milliseconds to minutes
-    
-    
+
+    last = len(fgreen1)
     # All columns containing florescence must be the same length to plot them
-    # This creates a standard length for all columns 
-    
-    # For each column, the true florescence data is interleaved such that every 3rd data point is the true florescence
-    # We need to "deinterleave" the data by taking each 3rd data point. 
-    # We have to identify what data point to start which is 25 because the first 25 data points are unreliable
-    # Next, we have to identify whether the 25th data point is the red, green, or isosbestic florescent channel. 
-    # Once the channel is identified, it follows a pattern of red, isosbestic, green...
-    
-    #Deinterleave the 3 channels for vole 1 (subject)
-    
-    #Deinterleave the 3 channels for vole 2 (partner or stranger) 
-    
+    # This creates a standard length for all columns
+
+    # For each column, the true flores. data is interleaved
+    # Every 3rd data point is the true flores.
+    # We need to "deinterleave" the data by taking each 3rd data point.
+    # Identify what data point to start (first 25 data points are unreliable)
+    # Then, identify if 25th data point is red, green, or isos. flores. channel
+    # Once channel is identified, it follows pattern of red, isos., green...
+    f1red = fred1[24: 2, last-2]
+    f1isosbestic = fgreen1[25: 2, last]
+    f1green = fgreen1[26: 2, last-1]
+    # Deinterleave the 3 channels for vole 1 (subject)
+    f2isosbestic = fgreen2[25: 2, last]
+    f2green = fgreen2[26: 2, last-1]
+    f2red = fred2[24: 2, last-2]
+    # Deinterleave the 3 channels for vole 2 (partner or stranger)
+    fTimeIsosbestic = ftime[25: 2, last]
+    fTimeGreen = ftime[26: 2, last-1]
+    fTimeRed = ftime[24: 2, last-2]
+    # Deinterleave the time column
+
     return fdata
