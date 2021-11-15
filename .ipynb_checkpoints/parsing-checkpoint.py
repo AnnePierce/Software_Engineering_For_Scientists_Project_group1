1df7c000f27a5dbc055b0fa4e6c71e8e7a727f02
wpcaroline <caho7448@colorado.edu>
2021-11-15 06:46:59 +0000

    tracking changes

--- .ipynb_checkpoints/parsing-checkpoint.py
+++ .ipynb_checkpoints/parsing-checkpoint.py
@@ -19,7 +19,7 @@ def file_reader(filename):
     ftime = f_time[0] / (60*1000) 
     # Converts ftime from milliseconds to minutes
     
-    
+    last = len(fgreen1)
     # All columns containing florescence must be the same length to plot them
     # This creates a standard length for all columns 
     
@@ -28,9 +28,17 @@ def file_reader(filename):
     # We have to identify what data point to start which is 25 because the first 25 data points are unreliable
     # Next, we have to identify whether the 25th data point is the red, green, or isosbestic florescent channel. 
     # Once the channel is identified, it follows a pattern of red, isosbestic, green...
-    
+    f1red = fred1(24:2, last-2)
+    f1isosbestic = fgreen1(25:2, last)
+    f1green = fgreen1(26:2, last-1)
     #Deinterleave the 3 channels for vole 1 (subject)
-    
+    f2isosbestic = fgreen2(25:2, last)
+    f2green = fgreen2(26:2, last-1)
+    f2red = fred2(24:2, last-2)
     #Deinterleave the 3 channels for vole 2 (partner or stranger) 
+    fTimeIsosbestic = ftime(25:2, last)
+    fTimeGreen = ftime(26:2, last-1)
+    fTimeRed = ftime(24:2, last-2)
+    #Deinterleave the time column
     
     return fdata
